7eeed3895ffd53f21714741f55eb189f04c358ad
onepieceD6 <kollamunna70@gmail.com>
2025-09-08 20:31:29 +0530

    env restarted

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/exchange-skills-live/' : '/',
   server: {
     host: "0.0.0.0", // Allow access from any IP
     port: 8080,
